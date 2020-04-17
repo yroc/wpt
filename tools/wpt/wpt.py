1575007fd6a15cce02b7abebb1b7b03a57da516d
Robert Ma <robertma@chromium.org>
2020-04-17 15:36:58 -0400

    [quic] Set up py3-only tools/quic and its venv
    This does not include the actual server, but only sets up the skeleton:
    * Introduce a "py3only" to commands.json
    * Create the skeleton of tools/quic and set up its venv
    
    The plan is to have `wpt serve` call `wpt servequic` (behind a flag).
    
    For more details, see
    https://github.com/web-platform-tests/rfcs/blob/master/rfcs/quic.md

--- tools/wpt/wpt.py
+++ tools/wpt/wpt.py
@@ -31,6 +31,7 @@ def load_commands():
                     "script": props["script"],
                     "parser": props.get("parser"),
                     "parse_known": props.get("parse_known", False),
+                    "py3only": props.get("py3only", False),
                     "help": props.get("help"),
                     "virtualenv": props.get("virtualenv", True),
                     "install": props.get("install", []),
@@ -40,7 +41,7 @@ def load_commands():
     return rv
 
 
-def parse_args(argv, commands = load_commands()):
+def parse_args(argv, commands=load_commands()):
     parser = argparse.ArgumentParser()
     parser.add_argument("--venv", action="store", help="Path to an existing virtualenv to use")
     parser.add_argument("--skip-venv-setup", action="store_true",
@@ -108,8 +109,10 @@ def create_complete_parser():
 
     return parser
 
+
 def venv_dir():
     return "_venv" + str(sys.version_info[0])
+
 
 def setup_virtualenv(path, skip_venv_setup, props):
     if skip_venv_setup and path is None:
